f01a26bdfc8f67bac64b3e60743d2f250bb37699
Ismail <herheulidzeismail980@gmail.com>
2023-01-02 17:21:45 +0500

    finally

---
+++ src/stores/movieStore.js
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia'
+import { ref, computed, watch } from 'vue'
+
+export const useMovieStore = defineStore('movieStore', () => {
+    const movies = ref([])
+    const activeTab = ref(2)
+
+    const moviesInLocalStorage = localStorage.getItem('movies')
+
+    if (moviesInLocalStorage) {
+        movies.value = JSON.parse(moviesInLocalStorage)._value
+    }
+
+    const watchedMovies = computed(() => {
+        return movies.value.filter(el => el.isWatched)
+    })
+
+    const setActiveTab = id => activeTab.value = id
+
+    const toggleWatched = id => {
+        const idx = movies.value.findIndex(el => el.id === id)
+        movies.value[idx].isWatched = !movies.value[idx].isWatched
+    }
+
+    const deleteMovie = id => movies.value = movies.value.filter(el => el.id !== id)
+
+    watch(() => movies, (state) => {
+        localStorage.setItem('movies', JSON.stringify(state))
+    }, {deep: true})
+
+    return { movies, activeTab, watchedMovies, setActiveTab, toggleWatched, deleteMovie }
+})
